223b2f8dd9c99f6a745cac795b4c9deeeecd7802
Matt Lima <matt@3-tone.com>
2024-04-12 10:34:48 -0400

    add route for this component to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FacetecComponent } from './facetec-component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FacetecComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
